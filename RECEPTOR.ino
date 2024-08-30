03c134b43b018f2320663928ed128f2906b1a6d7
Matheus Piassi <102124862+matheuspiassi@users.noreply.github.com>
2024-08-29 22:30:32 -0300

    Update RECEPTOR.ino

--- RECEPTOR.ino
+++ RECEPTOR.ino
@@ -1,6 +1,8 @@
 #include <HardwareSerial.h>
+#include <LiquidCrystal_I2C.h>
 
 HardwareSerial LoRaSerial(2); 
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 const int PIN_M0 = 19; 
 const int PIN_M1 = 18; 
@@ -10,6 +12,8 @@ void setup() {
   pinMode(PIN_M1, OUTPUT);
   digitalWrite(PIN_M0, LOW);
   digitalWrite(PIN_M1, HIGH);
+  lcd.init();
+  lcd.backlight();
 
   LoRaSerial.begin(9600, SERIAL_8N1, 17, 16); 
   Serial.begin(115200); 
@@ -19,5 +23,6 @@ void loop() {
   if (LoRaSerial.available()) {
     String receivedMessage = LoRaSerial.readStringUntil('\n');
     Serial.println("recebido: " + receivedMessage);
+    lcd.print(receivedMessage);
   }
 }
