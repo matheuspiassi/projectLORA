7ffb0c6bf19f6f9f253f924e0c5d1ead640e8d14
Matheus Piassi <102124862+matheuspiassi@users.noreply.github.com>
2024-09-03 21:06:59 -0300

    Update TRANSMISSOR.ino

--- TRANSMISSOR.ino
+++ TRANSMISSOR.ino
@@ -1,52 +1,85 @@
 #include <HardwareSerial.h>
 #include <TinyGPSPlus.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
 
-HardwareSerial LoRaSerial(2); // Use UART2 para a comunicação com o módulo LoRa
-
-const int PIN_M0 = 19; // Pino M0
-const int PIN_M1 = 18; // Pino M1
-
-TinyGPSPlus gps;
-
+HardwareSerial LoRaSerial(2);
 HardwareSerial ss(1);
 
+const int PIN_M0 = 19;
+const int PIN_M1 = 18;
+
+TinyGPSPlus gps;
+Adafruit_MPU6050 mpu;
+
 void setup() {
-  // Configuração dos pinos M0 e M1
   Serial.begin(115200);
   pinMode(PIN_M0, OUTPUT);
   pinMode(PIN_M1, OUTPUT);
+  
   ss.begin(9600, SERIAL_8N1, 25, 26);
-  // M0 HIGH e M1 LOW para modo transmissor
+  LoRaSerial.begin(9600, SERIAL_8N1, 17, 16);
+  
   digitalWrite(PIN_M0, HIGH);
   digitalWrite(PIN_M1, LOW);
 
-  // Inicializa a serial para comunicação com o módulo LoRa
-  LoRaSerial.begin(9600, SERIAL_8N1, 17, 16); // Configuração do UART2
-  Serial.begin(115200); // Serial para monitoramento
+  // Verifica se o módulo GY-521 (MPU6050) está conectado
+  if (!mpu.begin()) {
+    Serial.println("Falha ao conectar o módulo");
+    while (1) {
+      delay(10);
+    }
+  }
+
+  Serial.println("Módulo conectado");
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 }
 
 void loop() {
-   while (ss.available() > 0) {
-    gps.encode(ss.read());  // Decodifica os dados NMEA do GPS
+  while (ss.available() > 0) {
+    gps.encode(ss.read());
 
-    // Verifica se a localização foi atualizada com dados válidos
     if (gps.location.isUpdated()) {
+      // Captura os valores do GPS
       double latitude = gps.location.lat();
       double longitude = gps.location.lng();
+
+      // Captura os valores do giroscópio e acelerômetro
+      sensors_event_t a, g, temp;
+      mpu.getEvent(&a, &g, &temp);
+
+      // Cria a string JSON para enviar
       String dadosParaEnviar = "{\"longitude\": ";
-      dadosParaEnviar += String(longitude, 6);
+      dadosParaEnviar += String(longitude, 2);
       dadosParaEnviar += ", \"latitude\": ";
-      dadosParaEnviar += String(latitude, 6);  
+      dadosParaEnviar += String(latitude, 2);
+      dadosParaEnviar += ", \"accel_x\": ";
+      dadosParaEnviar += String(a.acceleration.x, 2);
+      dadosParaEnviar += ", \"accel_y\": ";
+      dadosParaEnviar += String(a.acceleration.y, 2);
+      dadosParaEnviar += ", \"accel_z\": ";
+      dadosParaEnviar += String(a.acceleration.z, 2);
+      dadosParaEnviar += "}";
 
+      // Imprime os valores no monitor serial
       Serial.print("Latitude: ");
-      Serial.println(latitude, 6);  // Exibe a latitude no Serial Monitor com 6 casas decimais
+      Serial.println(latitude, 2);
       Serial.print("Longitude: ");
-      Serial.println(longitude, 6); 
+      Serial.println(longitude, 2);
 
-      LoRaSerial.println(dadosParaEnviar); // Envia a mensagem via LoRa
-      delay(1000); // Aguarda 1 segundo antes de enviar novamente
+      Serial.print("Aceleração X: ");
+      Serial.print(a.acceleration.x);
+      Serial.print(", Y: ");
+      Serial.print(a.acceleration.y);
+      Serial.print(", Z: ");
+      Serial.print(a.acceleration.z);
+      Serial.println(" m/s^2");
+
+      // Envia os dados via LoRa
+      LoRaSerial.println(dadosParaEnviar);
+      delay(500);  // Atraso entre as transmissões
     }
-   }
+  }
 }
-
-//transmissor
